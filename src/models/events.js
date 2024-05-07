0ab7bf4ff2bcae4070f96b8fbc72dd1619e0a018
Armando García <art.gamir@gmail.com>
2024-05-06 22:25:01 -0600

    eventSchema added

--- src/models/events.js
+++ src/models/events.js
@@ -3,15 +3,20 @@ const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema({
   state: String,
-  country: String,
+  //country: String,
   zipCode: Number,
   city: String,
   street: String,
   exteriorNumber: String,
-  neighborhood: String,
+  interiorNumber: String,
+  neighbourhood: String,
 });
 
 const eventSchema = new mongoose.Schema({
+  eventName: {
+    type: String,
+    required: true,
+  },
   eventType: {
     type: String,
     required: true,
@@ -24,32 +29,54 @@ const eventSchema = new mongoose.Schema({
     type: addressSchema,
     required: true,
   },
-  status: {
-    type: String,
-    enum: {
-      values: ['pendiente', 'activo', 'rechazado', 'finalizado'],
-      message: '{VALUE} is not supported',
-    },
+  phoneClient: {
+    type: Number,
     required: true,
   },
-  eventConfirmationCode: {
-    type: Number,
-    // required: true,
-  },
-  idStripePayment: {
+  startHour: {
     type: String,
-    // required: true,
+    required: true,
   },
-  client: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users',
-    // required: true,
+  totalHours: {
+    type: Number,
+    required: true,
   },
-  musician: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users',
-    // required: true,
+  payment: {
+    type: Number,
+    required: true,
   },
+  isChecked: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+
+  // status: {
+  //   type: String,
+  //   enum: {
+  //     values: ['pendiente', 'activo', 'rechazado', 'finalizado'],
+  //     message: '{VALUE} is not supported',
+  //   },
+  //   required: true,
+  // },
+  // eventConfirmationCode: {
+  //   type: Number,
+  //   // required: true,
+  // },
+  // idStripePayment: {
+  //   type: String,
+  //   // required: true,
+  // },
+  // client: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'users',
+  //   // required: true,
+  // },
+  // musician: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'users',
+  //   // required: true,
+  // },
 });
 
 const Events = mongoose.model('Events', eventSchema);
