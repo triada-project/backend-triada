bcfce1ff4fc6bbe614cc869e8e1d01d585d666b9
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-12 20:43:34 -0600

    Stripe dependency was installed and routes were created for account creation. They work, pending connection with the flow

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const routes = require('./src/routes/index');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const db = require('./src/helpers/db');
+const stripe = require('stripe')(
+  // This is your test secret API key.
+  'sk_test_51PF8FkP5DUIoEtib5wFs7y3NDalsSF5eErPf7azm2n2YmJNsImhupj2l5sypVAmvDlV68N4TV12XLFrbFOYYJMMT00Kof8BlR8',
+  {
+    apiVersion: '2023-10-16',
+  },
+);
 
 db.connect();
 app.use(cors());
@@ -28,6 +35,57 @@ app.use('/', routes);
 
 app.use((resp, req, res, next) => {
   res.status(resp.status).send(resp.send);
+});
+
+app.post('/account_link', async (req, res) => {
+  try {
+    const { account } = req.body;
+
+    const accountLink = await stripe.accountLinks.create({
+      account: account,
+      return_url: `${req.headers.origin}/stepper/return/${account}`,
+      refresh_url: `${req.headers.origin}/stepper/refresh/${account}`,
+      type: 'account_onboarding',
+    });
+
+    res.json(accountLink);
+  } catch (error) {
+    console.error(
+      'An error occurred when calling the Stripe API to create an account link:',
+      error,
+    );
+    res.status(500);
+    res.send({ error: error.message });
+  }
+});
+
+app.post('/account', async (req, res) => {
+  try {
+    const account = await stripe.accounts.create({
+      controller: {
+        stripe_dashboard: {
+          type: 'express',
+        },
+        fees: {
+          payer: 'application',
+        },
+        losses: {
+          payments: 'application',
+        },
+      },
+    });
+
+    res.json({
+      account: account.id,
+    });
+  } catch (error) {
+    console.error(
+      'An error occurred when calling the Stripe API to create an account',
+      error,
+    );
+    res.status(500);
+    res.send({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
