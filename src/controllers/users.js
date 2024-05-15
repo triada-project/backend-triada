e86adb506b58dc9fc2a67f0b8c177ba39defd1cf
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-15 15:31:29 -0600

    Sendgrid was implemented to send verification mail, sending mail works, verification pending

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,30 @@
 const Users = require('../models/users');
 const jwt = require('../helpers/jwt');
+const twilio = require('twilio');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
+const client = require('twilio')(accountSid, authToken);
+
+const sendVerificationEmail = async (email) => {
+  try {
+    const verification = await client.verify.v2
+      .services(serviceSid)
+      .verifications.create({
+        to: email,
+        channel: 'email',
+        channelConfiguration: {
+          template_id: 'd-7596721b285940709a118c0a42f6f0d7',
+          from: 'Administracion@triada.rocks',
+          from_name: 'Administracion Triada',
+        },
+      });
+    return verification;
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+    throw new Error('Failed to send verification email');
+  }
+};
 
 module.exports = {
   getAll: async (req, res, next) => {
@@ -29,6 +54,11 @@ module.exports = {
   post: async (req, res, next) => {
     try {
       let user = await Users.create(req.body);
+      // Enviar el correo de verificación
+      const email = user.email;
+      const verification = await sendVerificationEmail(email);
+      console.log(`Verification SID: ${verification.sid}`);
+
       next({ status: 201, send: { msg: 'Usuario creado', data: { user } } });
     } catch (error) {
       next({ status: 400, send: { msg: 'Usuario no creado', err: error } });
