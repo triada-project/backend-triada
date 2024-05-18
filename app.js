52cf01883a868d5586943034f232532e99ef5778
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-17 18:21:07 -0600

    se unieron cambios de rama stripeConnect, ok

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const loggerStream = require('./src/utils/handleLogger');
 const cors = require('cors');
 const routes = require('./src/routes/index.js');
 const app = express();
-const { resolve } = require("path");
+const { resolve } = require('path');
 const PORT = process.env.PORT || 5000;
 const payMentController = require('./src/controllers/paymentController.js');
 const db = require('./src/helpers/db');
-const stripe = require('stripe')(
+const stripeConnect = require('stripe')(
   // This is your test secret API key.
   'sk_test_51PF8FkP5DUIoEtib5wFs7y3NDalsSF5eErPf7azm2n2YmJNsImhupj2l5sypVAmvDlV68N4TV12XLFrbFOYYJMMT00Kof8BlR8',
   {
@@ -18,15 +18,13 @@ const stripe = require('stripe')(
   },
 );
 const fileUpload = require('express-fileupload');
-const env = require("dotenv").config({ path: "./.env" });
+const env = require('dotenv').config({ path: './.env' });
 const mongoose = require('mongoose');
-const Events = require('./src/models/events.js')
+const Events = require('./src/models/events.js');
 
-
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-08-01',
 });
-
 
 db.connect();
 
@@ -35,7 +33,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(morgan('dev'));
-
 
 morganBody(app, {
   noColors: true,
@@ -52,19 +49,17 @@ app.use(
   }),
 );
 
-app.use('/',routes);
-
+app.use('/', routes);
 
 app.use((resp, req, res, next) => {
   res.status(resp.status).send(resp.send);
 });
 
-
 app.post('/account_link', async (req, res) => {
   try {
     const { account } = req.body;
 
-    const accountLink = await stripe.accountLinks.create({
+    const accountLink = await stripeConnect.accountLinks.create({
       account: account,
       return_url: `${req.headers.origin}/stepper/finalizar/${account}`,
       refresh_url: `${req.headers.origin}/stepper/refresh/${account}`,
@@ -84,7 +79,7 @@ app.post('/account_link', async (req, res) => {
 
 app.post('/account', async (req, res) => {
   try {
-    const account = await stripe.accounts.create({
+    const account = await stripeConnect.accounts.create({
       controller: {
         stripe_dashboard: {
           type: 'express',
@@ -113,26 +108,22 @@ app.post('/account', async (req, res) => {
 
 app.use(express.static(process.env.STATIC_DIR));
 
-app.get("/", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
+app.get('/', (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + '/index.html');
   res.sendFile(path);
 });
 
-
-app.get("/config", (req, res) => {
+app.get('/config', (req, res) => {
   res.send({
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   });
 });
 
-
-app.post("/create-payment-intent", async (req, res) => {
-  const {eventFee,_id} = req.body;
-  console.log('_id',_id);
-
+app.post('/create-payment-intent', async (req, res) => {
+  const { eventFee, _id } = req.body;
+  console.log('_id', _id);
 
   try {
-
     //8const event = await Events.findById(eventId);
 
     //console.log(event,'hola');
@@ -141,28 +132,25 @@ app.post("/create-payment-intent", async (req, res) => {
     //   return res.status(404).send({error:{message:'Evento no encontrado'}});
     // }
 
-
     const paymentIntent = await stripe.paymentIntents.create({
-      currency: "MXN",
+      currency: 'MXN',
       amount: eventFee,
       automatic_payment_methods: { enabled: true },
-      capture_method: "manual",
+      capture_method: 'manual',
     });
     //res.redirect('http://localhost:3000');
     // Send publishable key and PaymentIntent details to client
     res.send({
-      clientSecret: paymentIntent.client_secret
+      clientSecret: paymentIntent.client_secret,
     });
 
-
     const event = await Events.findById(_id);
-    console.log(event,'soy event');
+    console.log(event, 'soy event');
 
-    if(event){
-      event.idStripePayment=paymentIntent.client_secret
-      await event.save()
+    if (event) {
+      event.idStripePayment = paymentIntent.client_secret;
+      await event.save();
     }
-
   } catch (e) {
     return res.status(400).send({
       error: {
@@ -194,30 +182,31 @@ app.post("/create-payment-intent", async (req, res) => {
 //   }
 // });
 
-
 const capturePayment = async (paymentIntentId) => {
   try {
     const paymentIntent = await stripe.paymentIntents.capture(paymentIntentId);
-    console.log("Payment captured successfully:", paymentIntent);
+    console.log('Payment captured successfully:', paymentIntent);
     return paymentIntent;
   } catch (error) {
-    console.error("Error capturing payment:", error);
+    console.error('Error capturing payment:', error);
     throw error;
   }
 };
 
-
-app.post("/capture-payment", async (req, res) => {
+app.post('/capture-payment', async (req, res) => {
   const { paymentIntentId } = req.body;
   try {
     const capturedPayment = await capturePayment(paymentIntentId);
-    res.status(200).send({ message: "Payment captured successfully", payment: capturedPayment });
+    res
+      .status(200)
+      .send({
+        message: 'Payment captured successfully',
+        payment: capturedPayment,
+      });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
 });
-
-
 
 app.get('/', (req, res) => {
   res.send('Hola desde express');
@@ -225,5 +214,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
-
 });
